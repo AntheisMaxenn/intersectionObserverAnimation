e04a5be0244ddde05c421bb504f4d1c207d4dc7b
AntheisMaxenn <53502487+AntheisMaxenn@users.noreply.github.com>
2021-10-15 22:51:11 +1100

    Init Commit
    IntersectionObserver | Angular | SCROLL SNAP | SCSS + variables passes from view inline.
    
    Unable to get animate.css animation to work with the intersectionObserver yet, May revisit if able to implement.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { animation } from '@angular/animations';
+import { Component, HostListener, OnInit, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'intersectionObserverAnimation';
+
+  targets;
+  
+  constructor(private el: ElementRef){
+
+  }
+
+  ngOnInit(){
+  }
+  
+  ngAfterViewInit(){
+    this.targets = document.querySelectorAll('.animate');
+
+      const observer = new IntersectionObserver((entries) => {
+
+        entries.forEach(entry => {
+          
+          if(entry.intersectionRatio > 0) {
+            console.log("isIntersecting id: " + entry.target.id)
+            // entry.target.classList.add('animate__animated', 'animate__fadeInLeftBig');
+            entry.target.classList.add('animateMe');
+          } else {
+            console.log("Else id: " + entry.target.id)
+            // entry.target.classList.add('animationNone');
+            // entry.target.classList.remove('animate__animated', 'animate__fadeInLeftBig');
+            entry.target.classList.remove('animateMe');
+          }
+
+        })
+    })
+
+    this.targets.forEach(h1 => {
+        observer.observe(h1)
+    })
+
+
+
+    // this.targets.forEach(observer);
+  }
 }
